6931cf717aeb5d1962410f1270dfd375dc0e58ca
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-15 01:23:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,9 @@ def line(deli)
     puts "The line is currently empty."
   elsif deli.length > 0
     status = "The line is currently:"
-    deli.each_with_index {|name, index| status << " #{index+1}. #{name}"}
+    deli.each_with_index do |name, index| 
+      status << " #{index+1}. #{name}"
+    end
     puts status
     #puts "The line is currently: " + katz_deli.each_with_index.map {|name, index| "#{index+1}. #{name}"}.join(" ")
   end
