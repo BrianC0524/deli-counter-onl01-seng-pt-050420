59480ec23b306e15800188280f45f3084a808730
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-15 01:18:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 
 def take_a_number(deli, name)
   if katz_deli.length == 0
-    katz_deli.unshift("#{name}")
+    deli.unshift("#{name}")
   elsif katz_deli.length > 0
-    katz_deli << name
+    deli << name
   end
   
-  katz_deli.each_with_index.map {|name, pos| puts "Welcome, #{name}. You are number #{pos + 1} in line."}.reverse()
+  deli.each_with_index.map {|name, pos| puts "Welcome, #{name}. You are number #{pos + 1} in line."}.reverse()
   
 end
 
@@ -15,11 +15,11 @@ def line(deli)
   
   
   
-  if katz_deli.length == 0
+  if deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0
+  elsif deli.length > 0
     status = "The line is currently:"
-    katz_deli.each_with_index {|name, index| status << " #{index+1}. #{name}"}
+    deli.each_with_index {|name, index| status << " #{index+1}. #{name}"}
     puts status
     #puts "The line is currently: " + katz_deli.each_with_index.map {|name, index| "#{index+1}. #{name}"}.join(" ")
   end
